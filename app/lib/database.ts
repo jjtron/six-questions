987f9973b918f124fdc147407c6d457d1fe8afad
Gerald Petron <jjtron@Geralds-Mac-mini.local>
2024-02-12 20:02:50 -0500

    Develop database record keeping code

--- app/lib/database.ts
+++ app/lib/database.ts
@@ -66,28 +66,22 @@ const client = new Client({
   }
 
 /* SCRIPT TO CREATE TABLE "six_questions"
-    DROP TABLE IF EXISTS public.six_questions;
 
+    DROP TABLE IF EXISTS public.six_questions;
     CREATE TABLE IF NOT EXISTS public.six_questions
     (
-      id uuid NOT NULL,
-      who json NOT NULL,
-      what character varying(2000) COLLATE pg_catalog."default" NOT NULL,
-      "where" json NOT NULL,
-      "when" json NOT NULL,
-      why character varying(500) COLLATE pg_catalog."default" NOT NULL,
-      how character varying(1000) COLLATE pg_catalog."default" NOT NULL
-        
+        id uuid NOT NULL,
+        who json NOT NULL,
+        what character varying(2000) COLLATE pg_catalog."default" NOT NULL,
+        "where" integer NOT NULL,
+        "when" json NOT NULL,
+        why character varying(500) COLLATE pg_catalog."default" NOT NULL,
+        how character varying(1000) COLLATE pg_catalog."default" NOT NULL,
+        CONSTRAINT six_questions_pkey PRIMARY KEY (id)
     )
 
     TABLESPACE pg_default;
 
     ALTER TABLE IF EXISTS public.six_questions
         OWNER to postgres;
-
-    EXAMPLE SCRIPT FOR INSERTING DATA
-    INSERT INTO six_questions (id, who, what, "where", "when", why, how)
-    VALUES ('1274b03e-f668-4858-b169-62de4c596020',
-		'{"value0": 0}', 'value3', '{"value1": 0}',
-		'{"value2": 0}', 'value4', 'value5');
 */
