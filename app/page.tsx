2a6e75139da989d5ca6e91a4af6182e41495ada3
Gerald Petron <jjtron@Geralds-Mac-mini.local>
2024-02-16 08:58:56 -0500

    Develop with Tailwind css

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 "use client"
 
 import { Button } from "@/app/ui/button"
+import { lusitana } from '@/app/ui/fonts';
 
 export default function Home() {
   const showdatalink: string = "/records";
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-start p-24">
+      <p
+      className={`${lusitana.className} text-xl text-black md:text-3xl md:leading-normal`}
+          >This is just to try out a special font</p>
       <Button className="mt-4 w-full" buttontext={'CallAPI'} showdatalink={showdatalink}></Button>
     </main>
   );
