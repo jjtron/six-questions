1fa3b55574ce2ac81a4c843d572c77e037263cd5
Gerald Petron <jjtron@Geralds-Mac-mini.local>
2024-02-27 20:19:10 -0500

    Simplify strategy to highlight selected record

--- app/ui/records/view/table-places.tsx
+++ app/ui/records/view/table-places.tsx
@@ -6,28 +6,21 @@ import clsx from 'clsx';
 
 export function PlacesTable({records} : {records : any} ) {
 
-    let dynamicClassBase: any = [];
-    for (let i = 0; i < records.length; i++) {
-        dynamicClassBase.push("bg-sky-300");
-    }
-    const [dynamicClass, setDynamicClass] = useState(dynamicClassBase);
     const [buttonDisabled, setButtonDisabled] = useState("disabled-link");
-    const [recordid, setRecordId] = useState(0);
+    const [rowIndex, setRowIndex] = useState(-1);
+    const [selectedRecordId, setSelectedRecordId] = useState(-1);
 
     function handleClick(i: number, selectedRecordId: number) {
-        let dynamicClassFresh = dynamicClassBase.splice(0);
-        dynamicClassFresh.splice(i, 1, "bg-sky-600");
-        setRecordId(selectedRecordId);
+        setRowIndex(i);
+        setSelectedRecordId(selectedRecordId);
         setButtonDisabled("");
-        setDynamicClass(dynamicClassFresh);
-        console.log(selectedRecordId);
     }
 
     return (
         <form className="flex flex-col md:pl-2 bg-inherit">
             <div className="table w-full">
                 <div className="table-header-group">
-                    <div className="table-row bg-sky-250">
+                    <div className="table-row bg-sky-400">
                         <div className="table-cell pl-2 text-left font-bold border-tbl-1 border-slate-400 rounded-l-md">Place Name</div>
                         <div className="table-cell text-left font-bold border-y-1 border-slate-400">Street</div>
                         <div className="table-cell text-left font-bold border-y-1 border-slate-400">City</div>
@@ -37,7 +30,12 @@ export function PlacesTable({records} : {records : any} ) {
                 { records.map((record: any, i: number) => {
                 return (
                     <div key={i} className="table-row-group">
-                        <div className={clsx(`table-row ${dynamicClass[i]}`)} onClick={() => handleClick(i, record.id)} >
+                        <div className={clsx("table-row",
+                                            {"bg-sky-300" : i === rowIndex,
+                                             "bg-sky-200" : i !== rowIndex
+                                            })
+                                       }
+                                       onClick={() => handleClick(i, record.id)} >
                             <div className="table-cell pl-2 border-tbl-1 border-slate-400 rounded-l-md">{record.name}</div>
                             <div className="table-cell border-y-1 border-slate-400">{record.details.street}</div>
                             <div className="table-cell border-y-1 border-slate-400">{record.details.city}</div>
@@ -46,10 +44,10 @@ export function PlacesTable({records} : {records : any} ) {
                     </div>)
                 })}
             </div>
-            <div className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div className="flex min-h-screen flex-col items-center justify-between p-4">
                 <Button isDisabled={buttonDisabled}
                         cName={"w-24"}
-                        showdatalink={`/records/${recordid}/edit/places`}
+                        showdatalink={`/records/${selectedRecordId}/edit/places`}
                         buttontext={"Edit"} >
                 </Button>
             </div>
