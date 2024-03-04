1dc0df56e7f1bac12da840724649d1dadd68d43d
Gerald Petron <jjtron@Geralds-Mac-mini.local>
2024-03-03 20:44:14 -0500

    Get ridof useless console.log()

--- app/ui/records/view/view-people.tsx
+++ app/ui/records/view/view-people.tsx
@@ -5,6 +5,5 @@ import { PeopleTable } from "./table-people";
 
 export default async function Form({ query, currentPage } : { query: string, currentPage: number }) {
     const records = await fetchFilteredPeople(query, currentPage);
-    //console.log(records);
     return <PeopleTable records={records} />
 }
