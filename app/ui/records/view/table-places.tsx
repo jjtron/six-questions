3de66887e7b8eab9d87ea668f9e7c5c2995e70a1
Gerald Petron <jjtron@Geralds-Mac-mini.local>
2024-03-06 17:32:55 -0500

    Perfect the way of identifying the selected line to edit

--- app/ui/records/view/table-places.tsx
+++ app/ui/records/view/table-places.tsx
@@ -7,13 +7,12 @@ import clsx from 'clsx';
 export function PlacesTable({records} : {records : any} ) {
 
   const [buttonDisabled, setButtonDisabled] = useState("disabled-link");
-  const [rowIndex, setRowIndex] = useState(-1);
+  const [rowIndex, setRowIndex] = useState('-1');
   const [selectedRecordId, setSelectedRecordId] = useState(-1);
 
-  function handleClick(selectedRecordId: number) {
-    console.log(selectedRecordId);
+  function handleClick(selectedRecordId: string, recordid: number) {
       setRowIndex(selectedRecordId);
-      setSelectedRecordId(selectedRecordId);
+      setSelectedRecordId(recordid);
       setButtonDisabled("");
   }
 
@@ -24,8 +23,9 @@ export function PlacesTable({records} : {records : any} ) {
 
             return (
               group.map((record: any, j: number) => {
+                const lineId: string = (i + '') + (j + '');
                 return (
-                  <div key={group.length * i + j} className="w-full flex flex-col">
+                  <div key={lineId} className="w-full flex flex-col">
                     <p className={clsx("pl-2", {"hidden" : (j !== 0)})}>{record.type}</p>
                     <div className={clsx("flex flex-row bg-sky-400 rounded-md", {"hidden" : (j !== 0 || record.type !== "country")})}>
                       <div className="basis-1/2 pl-2 text-left font-bold border-tbl-1 border-slate-400 rounded-l-md">Country</div>
@@ -37,9 +37,9 @@ export function PlacesTable({records} : {records : any} ) {
                       <div className="basis-1/4 text-left font-bold border-y-1 border-slate-400">City</div>
                       <div className="basis-1/4 text-left font-bold border-trb-1 border-slate-400 rounded-r-md">State</div>
                     </div>
-                    <div className={clsx("flex flex-row rounded-md", { "bg-sky-300" : (group.length * i + j) === rowIndex, "bg-sky-200" : (group.length * i + j) !== rowIndex })}
-                        onClick={() => handleClick(group.length * i + j)} >
-                      <div className="basis-1/4 pl-2 border-tbl-1 border-slate-400 rounded-l-md">{record.name}{group.length * i + j}</div>
+                    <div className={clsx("flex flex-row rounded-md", { "bg-sky-300" : (lineId) === rowIndex, "bg-sky-200" : (lineId) !== rowIndex })}
+                        onClick={() => handleClick(lineId, record.id)} >
+                      <div className="basis-1/4 pl-2 border-tbl-1 border-slate-400 rounded-l-md">{record.name}</div>
                       <div className="basis-1/4 border-y-1 border-slate-400">{record.details.street}</div>
                       <div className="basis-1/4 border-y-1 border-slate-400">{record.details.city}</div>
                       <div className="basis-1/4 border-trb-1 border-slate-400 rounded-r-md">{record.details.state}</div>
