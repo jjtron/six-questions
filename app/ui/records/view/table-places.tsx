0862b30d08a8612f58d34d2947d18616df01d3e5
Gerald Petron <jjtron@Geralds-Mac-mini.local>
2024-03-05 21:22:43 -0500

    Prove concept that header cna be index 0, others can follow

--- app/ui/records/view/table-places.tsx
+++ app/ui/records/view/table-places.tsx
@@ -16,33 +16,51 @@ export function PlacesTable({records} : {records : any} ) {
         setButtonDisabled("");
     }
 
+    function showPlaceRecord (record: any, i: number) {
+        return (
+            <div className={clsx("table-row",
+                                {"bg-sky-300" : i === rowIndex,
+                                 "bg-sky-200" : i !== rowIndex
+                                })
+                           }
+                           onClick={() => handleClick(i, record.id)} >
+                <div className="table-cell pl-2 border-tbl-1 border-slate-400 rounded-l-md">{record.name}{i}</div>
+                <div className="table-cell border-y-1 border-slate-400">{record.details.street}</div>
+                <div className="table-cell border-y-1 border-slate-400">{record.details.city}</div>
+                <div className="table-cell border-trb-1 border-slate-400 rounded-r-md">{record.details.state}</div>
+            </div>
+        )
+    }
+
     return (
         <form className="md:pl-2 bg-inherit">
             <div className="table w-full">
-                <div className="table-header-group">
-                    <div className="table-row bg-sky-400">
-                        <div className="table-cell pl-2 text-left font-bold border-tbl-1 border-slate-400 rounded-l-md">Place Name</div>
-                        <div className="table-cell text-left font-bold border-y-1 border-slate-400">Street</div>
-                        <div className="table-cell text-left font-bold border-y-1 border-slate-400">City</div>
-                        <div className="table-cell text-left font-bold border-trb-1 border-slate-400 rounded-r-md">State</div>
-                    </div>
-                </div>
-                { records.map((record: any, i: number) => {
-                return (
-                    <div key={i} className="table-row-group">
-                        <div className={clsx("table-row",
-                                            {"bg-sky-300" : i === rowIndex,
-                                             "bg-sky-200" : i !== rowIndex
-                                            })
-                                       }
-                                       onClick={() => handleClick(i, record.id)} >
-                            <div className="table-cell pl-2 border-tbl-1 border-slate-400 rounded-l-md">{record.name}</div>
-                            <div className="table-cell border-y-1 border-slate-400">{record.details.street}</div>
-                            <div className="table-cell border-y-1 border-slate-400">{record.details.city}</div>
-                            <div className="table-cell border-trb-1 border-slate-400 rounded-r-md">{record.details.state}</div>
-                        </div>
-                    </div>)
-                })}
+                { 
+                    records.map((record: any, j: number) => {
+                        if (j === 0) {
+                            return (
+                                <div key={j} className="table-header-group">
+                                    <p className="pl-2 bg-gray-400 font-bold">{record.type}</p>
+                                    <div className="table-row bg-sky-400">
+                                        <div className="table-cell pl-2 text-left font-bold border-tbl-1 border-slate-400 rounded-l-md">Place Name</div>
+                                        <div className="table-cell text-left font-bold border-y-1 border-slate-400">Street</div>
+                                        <div className="table-cell text-left font-bold border-y-1 border-slate-400">City</div>
+                                        <div className="table-cell text-left font-bold border-trb-1 border-slate-400 rounded-r-md">State</div>
+                                    </div>
+                                </div>
+                            )
+                        } else if (j > 0 && j < (records.length)) {
+                            return (
+                                <div key={j} className="table-row-group">
+                                    {showPlaceRecord(records[j - 1], j)}
+                                </div>
+                            )
+                        }
+                    })
+                }
+                { 
+                    showPlaceRecord(records[records.length - 1], records.length)
+                }
             </div>
             <div className="flex flex-col items-center justify-between p-4">
                 <Button isDisabled={buttonDisabled}
