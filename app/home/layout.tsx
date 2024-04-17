1d2e0bd0a945e2a1408926b9ee112f3aed587379
Gerald Petron <jjtron@Geralds-Mac-mini.local>
2024-04-17 17:09:13 -0400

    Re-arrange layout to have a '/home' page

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "@/app/globals.css";
 import NavLinks from '@/app/ui/records/nav-links';
 import clsx from 'clsx';
 
