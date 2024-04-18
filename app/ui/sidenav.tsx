769b189c9b5c27209c0df679aa9fdc9c240f0e3d
Gerald Petron <jjtron@Geralds-Mac-mini.local>
2024-04-17 20:47:55 -0400

    Develop look and feel of logout button

--- app/ui/sidenav.tsx
+++ app/ui/sidenav.tsx
@@ -12,18 +12,14 @@ export default function SideNav() {
           }}
         >
           
-          <button className="flex h-[48px] w-full grow items-center rounded-md bg-gray-50 text-sm font-medium hover:bg-sky-100 hover:text-blue-600">
-            <div className="flex flex-row relative w-full">
-              <div><PowerIcon className="w-6 absolute left-0" /></div>
-              <div className="w-full text-center hidden md:block">Sign Out</div>
+          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md 
+                             bg-gray-50 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 
+                             md:flex-none md:justify-start md:p-0 p-2 px-3 border-1 border-gray-500" >
+            <div className="md:flex md:flex-row md:relative w-full">
+              <div><PowerIcon className="w-6 md:absolute md:left-0" /></div>
+              <div className="md:w-full md:text-center hidden md:block">Sign Out</div>
             </div>
           </button>
-          {/*
-          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
-            <PowerIcon className="w-6" />
-            <div className="hidden md:block">Sign Out</div>
-          </button>
-          */}
         </form>
       </div>
   );
