40436290784c4861bc8a86be623dcf34e59ab6bf
Gerald Petron <jjtron@Geralds-Mac-mini.local>
2024-04-23 10:58:05 -0400

    Spelling mistake

--- app/page.tsx
+++ app/page.tsx
@@ -13,9 +13,9 @@ export default function Page() {
           <div className={`${lusitana.className} md:text-3xl text-xl font-bold text-center w-full mb-4`}>Six Questions App</div>
           <div className={`${lusitana.className} text-xl text-black md:text-xl md:leading-normal md:px-28 px-6 text-justify`} >
               <p className="mb-3">This app is inspired by the famous six questions that one should ask of one&apos;s self before writing about an event.</p>
-              <p className="mb-6">Anyone using this app is free to add content; it is only meant to demonstrate my skills
-                 in creating a front end React app, with Remote Server Componenets (RSC), CRUD capability, a backend PostgreSQL database,
-                 on a Linux internet web server, secured by an SSL certificate (https), and protected by state-of-the art User Authentication.
+              <p className="mb-6">Anyone using this app is free to add content; it is only meant to demonstrate my skill
+                 in creating a front end React app, with backend React Server Components (RSCs), CRUD capability, backend PostgreSQL database,
+                 deployed on a Linux web server, secured by an SSL certificate (https), and protected by state-of-the art User Authentication and Session Management.
               </p>
           </div>
           <div className="flex flex-row items-center md:px-28 px-6">
