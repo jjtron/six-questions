43ac1665a50020a2da3362b4088f1de0e307702e
Gerald Petron <jjtron@Geralds-Mac-mini.local>
2024-06-12 08:17:53 -0400

    Another messy change . . .

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -9,25 +9,6 @@ import { insertAnswerRecord, insertPlaceRecord,
 //import { signIn } from '@/auth';
 import { AuthError } from 'next-auth';
 
-export async function authenticate(
-  prevState: string | undefined,
-  formData: FormData,
-) {
-  try {
-    console.log('hello submit');
-    //await signIn('credentials', formData);
-  } catch (error) {
-    if (error instanceof AuthError) {
-      switch (error.type) {
-        case 'CredentialsSignin':
-          return 'Invalid credentials.';
-        default:
-          return 'Something went wrong.';
-      }
-    }
-    throw error;
-  }
-}
 
 //////////////////////EVENT-TIME FUNCTIONS/////////////////////
 export type CreateEventTimeState = {
